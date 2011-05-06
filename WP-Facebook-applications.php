22d01cf5a6d94bc61a781e4bee0d1d8ac64340d9
Jeremy Herve <jeremy@tagada.hu>
2011-05-06 12:13:30 +0200

    Basic styles, facebook-like headings and fonts Main plugin file Facebook PHP SDK source

---
+++ WP-Facebook-applications.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP-Facebook applications
+Version: 0.1
+Plugin URI: http://jeremy.tagada.hu
+Description: Create custom tabs for your Facebook pages, hosted on your WordPress blog.
+Author: Jeremy Herve
+Author URI: http://jeremy.tagada.hu
+*/
+?>
