a5f69a976a03e89a5d77fb28fc3f503553c6002f
Jeremy Herve <jeremy@tagada.hu>
2011-09-15 20:30:18 +0200

    Remove unnecessary php tag close/open

--- wp-fb-applications.php
+++ wp-fb-applications.php
@@ -23,8 +23,6 @@ License: GPL2
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-?>
-<?php 
 
 define( 'WPFBAPPS_URL', plugin_dir_url(__FILE__) );
 
