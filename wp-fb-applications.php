db2153afcc5c75ef3039cfa2253261de36377e9e
Jeremy Herve <jeremy@tagada.hu>
2012-03-13 22:06:23 -0400

    Remove mention of GPL
    Short version is enough

--- wp-fb-applications.php
+++ wp-fb-applications.php
@@ -8,21 +8,6 @@ Author: Jeremy Herve
 Author URI: http://jeremyherve.com
 License: GPL2
 */
-/*  Copyright 2012 Jeremy Herve (email : jeremy@jeremyherve.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 
 define( 'WPFBAPPS_URL', plugin_dir_url(__FILE__) );
 
